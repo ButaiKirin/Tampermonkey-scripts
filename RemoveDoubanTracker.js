d0a34898191bd98d199e8ea7947023c5c3a7e590
Konnyaku <he@hee.ink>
2022-02-18 11:47:26 +0800

    Update RemoveDoubanTracker.js

--- RemoveDoubanTracker.js
+++ RemoveDoubanTracker.js
@@ -2,14 +2,12 @@
 // @name         Remove Douban Tracker
 // @name:zh-CN   移除豆瓣个人信息跟踪
 // @namespace    https://github.com/Ryauou/Tampermonkey-scripts
-// @version      0.1
+// @version      0.1.2
 // @description  移除豆瓣页面内隐藏的个人信息跟踪标识，防止发豆瓣截图被精准溯源水表。
 // @author       Konnyaku
 // @match        https://*.douban.com/*
-// @match        http://*.douban.com/*
 // @icon         https://www.douban.com/favicon.ico
-// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js
-// @require      https://ossweb-img.qq.com/images/js/jquery/jquery-3.6.0.min.js
+// @license      MIT
 // ==/UserScript==
 
 (function() {
